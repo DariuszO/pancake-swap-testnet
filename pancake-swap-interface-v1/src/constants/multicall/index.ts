c572cd129b8ddf8dfa92a09f46135cbfea707ed8
Deadjim <deadjim@pm.me>
2021-11-02 23:21:03 +0100

    Update index.ts

--- pancake-swap-interface-v1/src/constants/multicall/index.ts
+++ pancake-swap-interface-v1/src/constants/multicall/index.ts
@@ -2,6 +2,7 @@ import { ChainId } from '@pancakeswap-libs/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
   [ChainId.BSCTESTNET]: '0x0659fbd3Dc3076D94D767a22179FAC0b7e144Bb1'
 }
 
